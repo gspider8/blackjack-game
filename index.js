7157642b24381220bbf081209fd36b14aca4d4c1
gspider8 <83229711+gspider8@users.noreply.github.com>
2023-09-21 13:28:53 -0400

    Adding the button

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
 
-// 1. Declare a variable called message and assign its value to an empty string
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    hasBlackJack = true
-    message = "Woohoo! You've got Blackjack!"
-} else {
-    isAlive = false
-    message = "You're out of the game! :("
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        hasBlackJack = true
+        message = "Woohoo! You've got Blackjack!"
+    } else {
+        isAlive = false
+        message = "You're out of the game! :("
+    }
+    document.getElementById("message-el").textContent = message
+    console.log("Score: " + sum + " | " + message)
+     
 }
 
-// 3. Log it out!
-console.log("Score: " + sum + " | " + message)
+
