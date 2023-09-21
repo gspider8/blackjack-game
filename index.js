003c5e6e4ba4511ed7d3fd19f41adf3ab84bc754
gspider8 <83229711+gspider8@users.noreply.github.com>
2023-09-21 13:33:54 -0400

    Display Sum

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let firstCard = 15
 let secondCard = 11;
 let sum = firstCard + secondCard;
-
 let hasBlackJack = false
 let isAlive = true
-
 let message = ""
+let messageEl = document.getElementById("message-el")
+// 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    // 3. Render the sum on the page using this format -> "Sum: 14"
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -17,9 +19,10 @@ function startGame() {
         isAlive = false
         message = "You're out of the game! :("
     }
-    document.getElementById("message-el").textContent = message
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
     console.log("Score: " + sum + " | " + message)
-     
+    
 }
 
 
