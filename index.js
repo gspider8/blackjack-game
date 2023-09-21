3e7a631d16a9fc5e1363983d71642f6f19738168
gspider8 <83229711+gspider8@users.noreply.github.com>
2023-09-21 13:37:54 -0400

    sum changes

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
-let sumEl = document.getElementById("sum-el")
+// let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el")
 
 function startGame() {
     // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -18,11 +20,9 @@ function startGame() {
     } else {
         isAlive = false
         message = "You're out of the game! :("
-    }
+    }   
     messageEl.textContent = message
-    sumEl.textContent = "Sum: " + sum
-    console.log("Score: " + sum + " | " + message)
-    
+        
 }
 
 
