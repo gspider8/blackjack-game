da6061d36369af0d2e64681765b47927747162fe
gspider8 <83229711+gspider8@users.noreply.github.com>
2023-09-21 12:41:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let firstCard = 10;
+let firstCard = 15
 let secondCard = 11;
 let sum = firstCard + secondCard;
-console.log(sum)
 
-if (sum < 21) {
+let hasBlackJack = false
+let isAlive = true
+
+if (sum <= 20) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
     console.log("Woohoo! You've got Blackjack!")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! :(")
+    isAlive = false
 }
+
+
+// Cash out
+console.log(hasBlackJack)
